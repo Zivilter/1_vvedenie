a496aac13d5ca1e878ecaba750ebbcdae25edc59
Alekseeva AA <glance93@yandex.ru>
2016-12-01 22:23:33 +0300

    Задание 6

---
+++ Program16.cs
@@ -0,0 +1,20 @@
+﻿using System; //используем пространство имен System
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Количество параметров:");
+            Console.WriteLine(args.Length);
+            Console.WriteLine("Параметры:");
+            for(int i=0; i<args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
